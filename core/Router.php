d3fc00117d2b206f16b8499bbf289ccb14a1fdde
Sama <samasky.rostami@gmail.com>
2022-05-21 11:36:57 +0430

    add Response class Status code renderContact and post method

--- core/Router.php
+++ core/Router.php
@@ -5,14 +5,16 @@ namespace App\core;
 class Router
 {
     public Request $request;
+    public Response $response;
     protected array $routes =[];
 
     /**
      * @param Request $request
      */
-    public function __construct(Request $request)
+    public function __construct(Request $request, Response $response)
     {
         $this->request = $request;
+        $this->response = $response;
     }
 
 
@@ -21,13 +23,19 @@ class Router
         $this->routes['get'][$path] = $callback;
     }
 
+    public function post($path, $callback)
+    {
+        $this->routes['post'][$path] = $callback;
+    }
+
     public function resolve()
     {
         $path = $this->request->getPath();
         $method = $this->request->getMethod();
         $callback = $this->routes[$method][$path] ?? false;
         if ($callback === false) {
-            return 'Not found';
+            $this->response->setStatusCode(404);
+            return $this->renderContent("Not found");
         }
         if (is_string($callback)) {
             return $this->renderView($callback);
@@ -39,6 +47,12 @@ class Router
     {
         $layoutContent = $this->layoutContent();
         $viewContent = $this->renderOnlyView($view);
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+    }
+
+    public function renderContent ($viewContent)
+    {
+        $layoutContent = $this->layoutContent();
         return str_replace('{{content}}', $viewContent, $layoutContent);
     }
 
--- public/index.php
+++ public/index.php
@@ -6,6 +6,6 @@ use App\core\Application;
 $app = new Application(dirname(__DIR__));
 
 $app->router->get('/', 'home');
-
 $app->router->get('/contact', 'contact');
+
 $app->run();
